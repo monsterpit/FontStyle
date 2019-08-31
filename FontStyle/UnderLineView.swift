c268693b585f553ea095a17d9a18a0d48ea42755
MB <mb@mbs-imac.local>
2019-08-31 16:28:41 +0530

    NSConstraint and NSLayoutAnchor

--- FontStyle/UnderLineView.swift
+++ FontStyle/UnderLineView.swift
@@ -44,7 +44,58 @@ class UnderLineView : UIView{
         label.translatesAutoresizingMaskIntoConstraints = false
         
         
+        //NSLayoutConstraint
+        //        NSLayoutConstraint(item: label4,
+        //                           attribute: .leading,
+        //                           relatedBy: .equal,
+        //                           toItem: label4.superview,
+        //                           attribute: .leading,
+        //                           multiplier: 1,
+        //                           constant: 50).isActive = true
+        //
+        //
+        //        NSLayoutConstraint(item: label4,
+        //                           attribute: .trailing,
+        //                           relatedBy: .equal,
+        //                           toItem: label4.superview,
+        //                           attribute: .trailing,
+        //                           multiplier: 1,
+        //                           constant: -50).isActive = true
+        //
+        //        NSLayoutConstraint(item: label4,
+        //                           attribute: .top,
+        //                           relatedBy: .equal,
+        //                           toItem: label4.superview,
+        //                           attribute: .top,
+        //                           multiplier: 1,
+        //                           constant: 50).isActive = true
+        //
+        //        NSLayoutConstraint(item: label4,
+        //                           attribute: .bottom,
+        //                           relatedBy: .equal,
+        //                           toItem: label4.superview,
+        //                           attribute: .bottom,
+        //                           multiplier: 1,
+        //                           constant: -50).isActive = true
         
+        
+        //method 2  NSLayoutAnchor
+        
+//        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+//
+//        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+//
+//        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+//
+//        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        
+        label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50).isActive = true
+        
+        label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -50).isActive = true
+        
+        label.topAnchor.constraint(equalTo: self.topAnchor, constant: 50).isActive = true
+        
+        label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50).isActive = true
         
     }
     
