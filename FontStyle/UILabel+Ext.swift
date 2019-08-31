acb448f3b431b75a043a8ccd8157d037470fa026
MB <mb@MBs-iMac.local>
2019-08-31 20:47:45 +0530

    basicc structure done

---
+++ FontStyle/UILabel+Ext.swift
@@ -0,0 +1,37 @@
+//
+//  UILabel+Extension.swift
+//  FontStyle
+//
+//  Created by MB on 8/31/19.
+//  Copyright © 2019 MyGlamm. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel{
+    
+    /**
+     Takes in label and returns the array of text in that label.
+     
+     - Returns: A String in each line of label.
+     */
+    func getLinesArrayOfString() -> [String] {
+        
+        guard let text = text, let font = font else { return [] }
+        let ctFont = CTFontCreateWithName(font.fontName as CFString, font.pointSize, nil)
+        let attStr = NSMutableAttributedString(string: text)
+        attStr.addAttribute(kCTFontAttributeName as NSAttributedString.Key, value: ctFont, range: NSRange(location: 0, length: attStr.length))
+        let frameSetter = CTFramesetterCreateWithAttributedString(attStr as CFAttributedString)
+        let path = CGMutablePath()
+        path.addRect(CGRect(x: 0, y: 0, width: self.frame.size.width, height: CGFloat.greatestFiniteMagnitude), transform: .identity)
+        let frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, nil)
+        guard let lines = CTFrameGetLines(frame) as? [Any] else { return [] }
+        return lines.map { line in
+            let lineRef = line as! CTLine
+            let lineRange: CFRange = CTLineGetStringRange(lineRef)
+            let range = NSRange(location: lineRange.location, length: lineRange.length)
+            return (text as NSString).substring(with: range)
+        }
+    }
+    
+}
--- FontStyle/UnderLineView.swift
+++ FontStyle/UnderLineView.swift
@@ -10,95 +10,106 @@ import UIKit
 
 class UnderLineView : UIView{
     
-    private var label = UILabel()
+    private var labelText = UILabel()
     
-    
-    var labelText = ""{
-        didSet {
-            label.text = labelText
-        }
-    }
-    
-    var alignment : NSTextAlignment = .natural{
-        didSet{
-            label.textAlignment = alignment
-        }
-    }
-    
+    private var underline_Views : [UIView] = []
+
     
     override init(frame: CGRect) {
         super.init(frame: CGRect.zero)
+        setupLabel()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        setupLabel()
     }
+    
+    
+
     
     
     
     private func setupLabel(){
         
+       // setupTextForLabel()
+        
+        //backgroundColor = .red
+        
+        labelText.numberOfLines = 0
+        
+        
+        
+        
         //Add label
-        addSubview(label)
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(labelText)
         
         
-        //NSLayoutConstraint
-        //        NSLayoutConstraint(item: label4,
-        //                           attribute: .leading,
-        //                           relatedBy: .equal,
-        //                           toItem: label4.superview,
-        //                           attribute: .leading,
-        //                           multiplier: 1,
-        //                           constant: 50).isActive = true
-        //
-        //
-        //        NSLayoutConstraint(item: label4,
-        //                           attribute: .trailing,
-        //                           relatedBy: .equal,
-        //                           toItem: label4.superview,
-        //                           attribute: .trailing,
-        //                           multiplier: 1,
-        //                           constant: -50).isActive = true
-        //
-        //        NSLayoutConstraint(item: label4,
-        //                           attribute: .top,
-        //                           relatedBy: .equal,
-        //                           toItem: label4.superview,
-        //                           attribute: .top,
-        //                           multiplier: 1,
-        //                           constant: 50).isActive = true
-        //
-        //        NSLayoutConstraint(item: label4,
-        //                           attribute: .bottom,
-        //                           relatedBy: .equal,
-        //                           toItem: label4.superview,
-        //                           attribute: .bottom,
-        //                           multiplier: 1,
-        //                           constant: -50).isActive = true
+        labelText.translatesAutoresizingMaskIntoConstraints = false
+        
+        labelText.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        
+        labelText.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+        
+        labelText.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        
+        labelText.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
         
         
-        //method 2  NSLayoutAnchor
-        
-//        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-//
-//        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-//
-//        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-//
-//        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
-        
-        label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 50).isActive = true
-        
-        label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -50).isActive = true
-        
-        label.topAnchor.constraint(equalTo: self.topAnchor, constant: 50).isActive = true
-        
-        label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50).isActive = true
         
     }
     
     
+    func setupTextForLabel(withText text : String? = "",withFont font : UIFont = UIFont.systemFont(ofSize: 15),withColor color : UIColor = .black,withAlignment alignment : NSTextAlignment? = .natural){
+        
+        labelText.font = font
+        labelText.text = text
+        labelText.textColor = color
+        labelText.textAlignment = alignment!
+        
+    
+    }
+
+    
+    
+    
+    /**
+     Takes in color and creates the underline with specified color. Use in viewDidLayoutSubViews or on tap events
+     
+     - Parameters:
+     - withColor: The color of the underline default colorr is pink
+     - withHeight: The height of the underline default height is labelText.font.descender
+
+     */
+    func createUnderLineViews(withColor color : UIColor = #colorLiteral(red: 0.9891870618, green: 0.7692108154, blue: 0.7603701353, alpha: 1) ,withHeight height : CGFloat = 0){
+
+        let lines = labelText.getLinesArrayOfString()
+
+        print(lines)
+
+        
+        for (index,line) in lines.enumerated(){
+            
+            let lineSize = (line as NSString).size(withAttributes: [.font: labelText.font!])
+
+            
+            let underLine_View : UIView = UIView(frame: CGRect(x: labelText.frame.minX, y: ( ( labelText.frame.minY  + (labelText.font.lineHeight ) * CGFloat(index+1) ) - labelText.font.leading ), width: lineSize.width, height: (height != 0 ? height : (labelText.font.descender * CGFloat(1.5) ))))
+            
+            underLine_View.backgroundColor = color
+            
+            
+            
+            addSubview(underLine_View)
+            
+            //label1.layer.zPosition = 1
+            
+            bringSubviewToFront(labelText)
+            
+            
+            
+        }
+        
+    
+    }
     
 }
